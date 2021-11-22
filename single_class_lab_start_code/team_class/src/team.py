b5c8ceb7dfe661284590cdb91942ffecff54f7a6
Campbell Forsyth <camsh69@gmail.com>
2021-11-22 14:07:32 +0000

    completed single class lab

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,18 @@
+class Team:
+    def __init__(self, name, players, coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+        self.points = 0
+
+    def add_player(self, name):
+        self.players.append(name)
+
+    def has_player(self, player):
+        return player in self.players
+
+    def play_game(self, result):
+        if result:
+            self.points += 3
+            
+
